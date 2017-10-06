adf34176996fbb5021dfb1afe15774d756b6168b
mouse <503673140@qq.com>
2017-10-06 13:11:54 +0800

    第二次实验

--- src/实验3/main.java
+++ src/实验3/main.java
@@ -4,10 +4,10 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
-		//ChoclateBoiler.getInstance();
-		//ChoclateBoiler.fill();
-		//ChoclateBoiler.boil();
-		//ChoclateBoiler.drain();
+		ChoclateBoiler.getInstance();
+		ChoclateBoiler.fill();
+		ChoclateBoiler.boil();
+		ChoclateBoiler.drain();
 		first_question a=new first_question();
 		System.out.println(a.getClasses());
 	}
