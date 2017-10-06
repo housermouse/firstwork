2af9efb1feb1fbb1687c33466e1fa968799577b6
mouse <503673140@qq.com>
2017-10-06 13:30:41 +0800

    第一次实验

---
+++ src/secondtest/main.java
@@ -0,0 +1,14 @@
+package secondtest;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		ChoclateBoiler.getInstance();
+		ChoclateBoiler.fill();
+		ChoclateBoiler.boil();
+		ChoclateBoiler.drain();
+		first_question a=new first_question();
+		System.out.println(a.getClasses());
+	}
+}
